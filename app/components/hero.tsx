dc9c6e741157598dae1e51ee5d3a5a55634c74c3
Dantegro <dan@dantegro.com>
2026-05-22 20:51:47 +0100

    Create hero component and clean up template

---
+++ app/components/hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <section>
+            <h1 className="text-4xl font-bold">Dantegro</h1>
+            <p>Hello - I'm Datengro, a full-stack web developer building modern websites and web apps for small businesses.
+            </p>
+        </section>
+    )
+}
