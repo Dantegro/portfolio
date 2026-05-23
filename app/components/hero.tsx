25d39d743c19378cc05d4e0a06dbd09e69e0180f
Dantegro <dan@dantegro.com>
2026-05-23 18:54:02 +0100

    Add hero flex layout with profie image

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 export default function Hero() {
     return (
         <section>
-            <h1 className="text-4xl font-bold">Dantegro</h1>
-            <p>Hello - I'm Datengro, a full-stack web developer building modern websites and web apps for small businesses.
-            </p>
+            <div className="flex gap-8">
+                <div>
+                    <Image className="rounded-full" src="/images/profile-picture.webp" alt="Profile Picture" width={600} height={600} />
+                </div>
+                <div>
+                    <h1 className="text-3xl font-bold">Dantegro</h1>
+                    <p className="mt-5">Hello - I'm Datengro, a full-stack web developer building modern websites and web apps for small businesses.
+                    </p>
+                </div>
+            </div>
+
         </section>
     )
 }
