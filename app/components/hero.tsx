f74574fe6d5d3c794daeba3ade1654301e3b17b6
Dantegro <dan@dantegro.com>
2026-07-04 11:33:34 +0100

    Update Hero component with new profile image and content; enhance ServiceIntro layout and messaging

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,13 +5,13 @@ export default function Hero() {
         <section>
             <div className="flex gap-8">
                 <div>
-                    <Image className="rounded-full" src="/images/profile-picture.webp" alt="Profile Picture" width={600} height={600} />
+                    <Image className="rounded-full" src="/images/profile-picture.png" alt="Profile Picture" width={400} height={400} />
                 </div>
                 <div>
-                    <h1 className="text-3xl font-bold">Dantegro</h1>
+                    <h1 className="text-3xl font-bold">Dan Nguyen</h1>
 
                     <div className="mt-4 space-y-4">
-                        <p> Hi, I'm Dan. I build and redesign full-stack websites and web apps for small businesses.
+                        <p>Web developer for small businesses. Helping visitors become customers with websites and web apps that don’t just look good — they solve real business problems.
                         </p>
                     </div>
                 </div>
--- app/components/service-intro.tsx
+++ app/components/service-intro.tsx
@@ -1,9 +1,14 @@
 export default function ServiceIntro() {
     return (
-        <section>
-            <h2 className="text-2xl font-bold mb-4">What I Do</h2>
-            <p className="mb-4">I build full-stack websites and web apps with Next.js, TypeScript, Supabase/PostgreSQL, and Vercel.</p>
-            <p>Small businesses deserve better websites and a stronger online presence. That's why I build them. Websites that are cleaner, more secure, and built to attract more customers and help you succeed online.</p>
+        <section className="max-w-2xl mx-auto px-4 py-8">
+            <div className="space-y-4">
+                <p>
+                    Small businesses deserve better websites and a stronger online presence.
+                </p>
+                <p>
+                    That's why I build them. Websites that are focused on user experience, performance, security and helping the right customers find your business online.
+                </p>
+            </div>
         </section>
     )
 }
